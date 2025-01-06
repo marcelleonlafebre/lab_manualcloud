1f3cd635e426383bb0acb7ce6ff1f0bf45a0116e
marcelleonlafebre <39922158+marcelleonlafebre@users.noreply.github.com>
2025-01-05 19:46:45 -0500

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -36,17 +36,13 @@ LOCATION 's3://redshift-downloads/TPC-DS/2.13/1TB/item/'
 TBLPROPERTIES ( 'classification' = 'csv', 'write.compression' = 'GZIP'
 )
 
---table inventory
-create external table inventory
-(
- inv_date_sk int , 
-  inv_item_sk int ,
-  inv_warehouse_sk int ,
-  inv_quantity_on_hand int )
+--table warehouse
+create external table warehouse
+( w_warehouse_sk int, w_warehouse_id char(16) , w_warehouse_name varchar(20) , w_warehouse_sq_ft int , w_street_number char(10) , w_street_name varchar(60) , w_street_type char(15) , w_suite_number char(10) , w_city varchar(60) , w_county varchar(30) , w_state char(2) , w_zip char(10) , w_country varchar(20) , w_gmt_offset decimal(5,2) )
 ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe'
 WITH SERDEPROPERTIES ('field.delim' = '|')
 STORED AS INPUTFORMAT 'org.apache.hadoop.mapred.TextInputFormat'
 OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
-LOCATION 's3://redshift-downloads/TPC-DS/2.13/1TB/inventory/'
+LOCATION 's3://redshift-downloads/TPC-DS/2.13/1TB/warehouse/'
 TBLPROPERTIES ( 'classification' = 'csv', 'write.compression' = 'GZIP'
 )
